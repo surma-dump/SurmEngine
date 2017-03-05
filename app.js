f657d16fa477352c9d1cf0e6fe4bbdf389bff7c9
Surma <surma@surma.link>
2017-03-05 22:39:57 +0000

    colors!

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl2');
 let last = performance.now();
 let stop = false;
+window._gl = gl;
 
 const ro = new ResizeObserver(entries => {
-  gl.canvas.width = entries[0].contentRect.width;
-  gl.canvas.height = entries[0].contentRect.height;
+  gl.canvas.width = entries[0].contentRect.width * window.devicePixelRatio;
+  gl.canvas.height = entries[0].contentRect.height * window.devicePixelRatio;
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  console.log(`Setting render size to ${gl.canvas.width}x${gl.canvas.height}`);
 });
 ro.observe(gl.canvas);
 
@@ -15,7 +17,7 @@ function createShader(gl, type, source) {
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.log(`Error compiling shader: ${gl.getShaderInfoLog(shader)}`);
+    console.log(`Error compiling ${type==gl.VERTEX_SHADER?'vertex':'fragment'} shader: ${gl.getShaderInfoLog(shader)}`);
     return null
   }
   return shader;
@@ -31,16 +33,28 @@ function setup(gl) {
     -0.5, -0.5, 0,
     0.5, -0.5, 0
   ]), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(vertexVBO, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vertexVBO);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(0);
+  const colorVBO = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1
+  ]), gl.STATIC_DRAW);
+  gl.vertexAttribPointer(1, 3, gl.FLOAT, true, 0, 0);
+  gl.enableVertexAttribArray(1);
 
   const vShader = createShader(gl, gl.VERTEX_SHADER,
     `#version 300 es
 
-    in vec3 coords;
+    in vec3 in_coords;
+    in vec3 in_color;
     uniform vec3 offset;
+    out vec4 color;
     void main(void) {
-      gl_Position = vec4(coords + offset, 1);
+      gl_Position = vec4(in_coords + offset, 1);
+      color = vec4(in_color, 1);
     }
   `);
   if (!vShader) {
@@ -52,9 +66,10 @@ function setup(gl) {
     `#version 300 es
     precision highp float;
 
-    out vec4 color;
+    in vec4 color;
+    out vec4 out_color;
     void main(void) {
-      color = vec4(0, 0, 1, 1);
+      out_color = color;
     }
   `);
   if (!fShader) {
@@ -93,10 +108,11 @@ function loop(gl, data) {
   gl.drawArrays(gl.TRIANGLES, 0, 3);
   gl.uniform3f(data.uniforms.offset, 0.5, 0, 0);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.uniform3f(data.uniforms.offset, -0.5, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 function start() {
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   const data = setup(gl);
   requestAnimationFrame(function f() {
     const now = performance.now();
