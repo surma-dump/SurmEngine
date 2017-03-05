1e792c16057b1e9e95dbeb972facab5f2490db74
Surma <surma@surma.link>
2017-03-05 19:47:45 +0000

    Uniforms

--- app.js
+++ app.js
@@ -21,28 +21,26 @@ function createShader(gl, type, source) {
   return shader;
 }
 
-function loop(gl, now, last) {
-  gl.clearBufferfv(gl.COLOR, 0, new Float32Array([1, 0, 0, 1]));
-
+function setup(gl) {
   const vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
-  const vbo = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  const vertexVBO = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexVBO);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
     0, 0.5, 0,
     -0.5, -0.5, 0,
     0.5, -0.5, 0
   ]), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(vbo, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vbo);
+  gl.vertexAttribPointer(vertexVBO, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vertexVBO);
 
   const vShader = createShader(gl, gl.VERTEX_SHADER,
     `#version 300 es
 
     in vec3 coords;
-
+    uniform vec3 offset;
     void main(void) {
-      gl_Position = vec4(coords, 1);
+      gl_Position = vec4(coords + offset, 1);
     }
   `);
   if (!vShader) {
@@ -55,9 +53,8 @@ function loop(gl, now, last) {
     precision highp float;
 
     out vec4 color;
-
     void main(void) {
-      color = vec4(0, 1, 0, 1);
+      color = vec4(0, 0, 1, 1);
     }
   `);
   if (!fShader) {
@@ -68,7 +65,7 @@ function loop(gl, now, last) {
   const program = gl.createProgram();
   gl.attachShader(program, fShader);
   gl.attachShader(program, vShader);
-  gl.bindAttribLocation(program, vbo, "coords");
+  gl.bindAttribLocation(program, vertexVBO, 'coords');
   gl.linkProgram(program);
   if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     stop = true;
@@ -82,16 +79,29 @@ function loop(gl, now, last) {
     return;
   }
   gl.useProgram(program);
+  const offset = gl.getUniformLocation(program, 'offset');
+  return {
+    uniforms: {
+      offset
+    }
+  }
+}
 
+function loop(gl, data) {
+  gl.clearBufferfv(gl.COLOR, 0, new Float32Array([1, 0, 0, 1]));
+  gl.uniform3f(data.uniforms.offset, 0, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.uniform3f(data.uniforms.offset, 0.5, 0, 0);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 function start() {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  const data = setup(gl);
   requestAnimationFrame(function f() {
     const now = performance.now();
     const fps = 1000/(now - last);
-    loop(gl, now, last);
+    loop(gl, data, now, last);
     last = now;
     if(!stop) requestAnimationFrame(f);
   });
