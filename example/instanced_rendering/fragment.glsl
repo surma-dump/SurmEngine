f6c555087ef141da9c6a77ab9612ce0fd4841d4a
Surma <surma@surma.link>
2017-04-04 16:04:20 +0900

    Lol fix lighting

--- example/instanced_rendering/fragment.glsl
+++ example/instanced_rendering/fragment.glsl
@@ -8,6 +8,6 @@ out vec4 out_color;
 void main() {
   vec4 black = vec4(vec3(0.0), 1.0);
   out_color = color;
-  float lambert = clamp(dot(normalize(-light), normalize(normal)), 0.05, 1.0);
+  float lambert = clamp(dot(normalize(light), normalize(normal)), 0.05, 1.0);
   out_color = mix(black, color, lambert);
 }
