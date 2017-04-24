dfb83bba4cf290aac4cfefe0a409cabd89db0f57
Surma <surma@surma.link>
2017-04-24 13:29:28 +0100

    Use array of samplers as FPS fix in mipmap demo

--- example/mipmapping/fragment.glsl
+++ example/mipmapping/fragment.glsl
@@ -1,10 +1,11 @@
 #version 300 es
 precision highp float;
 in vec2 uv;
-uniform sampler2D sampler;
+uniform sampler2D samplers[4];
+uniform int samplerID;
 out vec4 out_color;
 
 void main() {
   vec4 black = vec4(vec3(0.0), 1.0);
-  out_color = texture(sampler, uv);
+  out_color = texture(samplers[samplerID], uv);
 }
