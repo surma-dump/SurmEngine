ffb1c18bc1f11d94f5ccbc9b4bdb4f1a5d081988
Surma <surma@surma.link>
2017-05-16 01:00:54 +0100

    Build update functions into scene graph

--- example/postprocessing/postprocessing.fragment.glsl
+++ example/postprocessing/postprocessing.fragment.glsl
@@ -6,7 +6,7 @@ uniform float time;
 out vec4 out_color;
 
 void main() {
-  vec2 offset = vec2(sin(time*2.0*3.14/1000.0 + uv.y*30.0), sin(time*2.0*3.14/1000.0 + uv.x*18.0));
+  vec2 offset = vec2(sin(time*2.0*3.14/1000.0 + uv.y*10.0), sin(time*2.0*3.14/1000.0 + uv.x*18.0));
   vec4 texel = texture(sampler, uv + offset * 0.005);
   out_color = texel;
 }
